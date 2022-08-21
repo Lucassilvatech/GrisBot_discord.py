415b88ca94baee545e2bc3fc1cd6e6f693b65cce
Lucas Silva <LucasSilvaCode@gmail.com>
2022-08-20 23:00:20 -0300

    solucionando erros de chamada do bot

--- gris.py
+++ gris.py
@@ -6,13 +6,17 @@ import os
 
 bot = commands.Bot(command_prefix="!", intents=Intents.all())
 
-for file in os.listdir("commands"):
-    if file.endswith(".py"):
-        cog = file[:-3]
-        bot.load_extension(f"commands.{cog}")
+
+def load_cogs(bot):
+    bot.load_extension("gerenciadores")
+
+    for file in os.listdir("commands"):
+        if file.endswith(".py"):
+            cog = file[:-3]
+            bot.load_extension(f"commands.{cog}")
 
 
-bot.load_extension("gerenciadores")
+load_cogs(bot)
 
 
 load_dotenv()
